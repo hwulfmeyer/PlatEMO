6e9aea5a2176f67b84c24e8bae8543be783b898f
hwulfmeyer <hans-martin.wulfmeyer@st.ovgu.de>
2021-08-26 09:22:17 +0200

    Update exp1.m

--- PlatEMO/Algorithms/HH_Evaluation/exp1.m
+++ PlatEMO/Algorithms/HH_Evaluation/exp1.m
@@ -17,7 +17,7 @@ for probi = 1 : size(problems,2)
     end
 end
 
-for k = 1 : size(experimentconfigurations,2)
+parfor k = 1 : size(experimentconfigurations,2)
     prob = experimentconfigurations{1,k}{1};
     probD = experimentconfigurations{1,k}{2};
     i_exp = experimentconfigurations{1,k}{3};
@@ -25,8 +25,13 @@ for k = 1 : size(experimentconfigurations,2)
     timerVal = tic;
     [Dec,Obj] = platemo('algorithm',@GA,'problem',{@HHProblem,prob,20,2000,probD,hhRepitions},'D',10,'N',100,'maxFE',10000,'save',10);
     timeToFinish = toc(timerVal);
-    save(filename, 'Dec', 'Obj', 'timeToFinish', '-mat');
+    parsave(filename, Dec, Obj, timeToFinish);
 end
+
+function parsave(fname,a,b,c)
+  save(fname, 'a', 'b', 'c','-mat')
+end
+
 
 
 
