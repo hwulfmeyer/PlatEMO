da7ad2510927474594644dfc6d9ccab8815ee347
hwulfmeyer <hans-martin.wulfmeyer@st.ovgu.de>
2021-08-26 09:18:58 +0200

    better parallel running

--- PlatEMO/Algorithms/HH_Evaluation/exp1.m
+++ PlatEMO/Algorithms/HH_Evaluation/exp1.m
@@ -1,23 +1,32 @@
-problems = {{@DTLZ1,5}};   %,{@WFG1, 12},{@WFG1, 12} {@DTLZ2, 40}, {@DTLZ3, 5}, {@WFG1, 12}, {@WFG2, 12}, {@WFG3, 50}, {@WFG4, 50}};
-expRepitions = 1;
+problems = {{@DTLZ1,5}, {@WFG1, 12}};   %,{@WFG1, 12},{@WFG1, 12} {@DTLZ2, 40}, {@DTLZ3, 5}, {@WFG1, 12}, {@WFG2, 12}, {@WFG3, 50}, {@WFG4, 50}};
+expRepitions = 5;
 hhRepitions = 11;
-warning('off', 'all')
+
+experimentconfigurations = cell(1,size(problems,2)*expRepitions);
+
 for probi = 1 : size(problems,2)
-    decRes = cell(1,expRepitions);
-    objRes = cell(1,expRepitions);
-    
     prob = problems{probi}{1};
     probD = problems{probi}{2};
     probstr = func2str(prob);
-    disp(probstr);
-    
-    timerVal = tic;
-    for i = 1 : expRepitions
-        [Dec,Obj] = platemo('algorithm',@GA,'problem',{@HHProblem,prob,20,2000,probD,hhRepitions},'D',10,'N',100,'maxFE',10000,'save',1);
-        objRes{1,i} = Obj;
-        decRes{1,i} = Dec;
-    end
-    timeToFinish = toc(timerVal);
 
-    save("Algorithms\HH_Evaluation\Data\results_exp1_" + probstr + "_.mat", 'decRes', 'objRes', 'timeToFinish', '-mat');
+    for i = 1 : expRepitions
+        filename = "Algorithms\HH_Evaluation\Data\results_exp1_" + probstr + "_" + i + "_.mat";
+        if ~isfile(filename)
+            experimentconfigurations{1, i + (probi-1)*expRepitions} = {prob, probD, i, filename};
+        end
+    end
 end
+
+for k = 1 : size(experimentconfigurations,2)
+    prob = experimentconfigurations{1,k}{1};
+    probD = experimentconfigurations{1,k}{2};
+    i_exp = experimentconfigurations{1,k}{3};
+    filename = experimentconfigurations{1,k}{4};
+    timerVal = tic;
+    [Dec,Obj] = platemo('algorithm',@GA,'problem',{@HHProblem,prob,20,2000,probD,hhRepitions},'D',10,'N',100,'maxFE',10000,'save',10);
+    timeToFinish = toc(timerVal);
+    save(filename, 'Dec', 'Obj', 'timeToFinish', '-mat');
+end
+
+
+
